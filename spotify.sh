e41c7f26e2e6d1a0efae9c64243647c6c5e957e5
Jordan Eisenman <joeisenman@DESKTOP-6OSVURI>
2023-11-16 12:51:53 -0500

    secrets binded

---
+++ spotify.sh
@@ -0,0 +1,3 @@
+curl -X POST "https://accounts.spotify.com/api/token" \
+     -H "Content-Type: application/x-www-form-urlencoded" \
+     -d "grant_type=client_credentials&client_id=7a5508ebd6db4900b938c8fd751b1cc9&client_secret=0809922fe37f457bb7c792288aa623eb"
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
+#include <cstdlib>
 #include "spotify/spotify.h"
+#include "curl/curl.h"
 using namespace std;
 
 int main() {
-    cout << "Hello from main\n";
-    spotifyFunction(); // Now you can call the function
+    const char* clientId = getenv("CLIENT_ID");
+    if (clientId != nullptr) {
+        cout << "Client ID: " << clientId << endl;
+    } else {
+        cout << " Client ID not set in environment" << endl;
+    }
+
+	cout << "env imported\n";
+	cout << "Hello again\n";
+	spotifyFunction(); // Now you can call the function
     return 0;
 }
 
